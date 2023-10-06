be0621df4fa9e781ba8707da8b4c54b4a3844cdc
uink45 <79078981+uink45@users.noreply.github.com>
2023-10-06 22:34:45 +1000

    Update code

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let overlay = document.querySelector('#overlay');
 let inputField = document.getElementById('openai-key');
 let darkModeToggle = document.getElementById('dark-mode');
 let systemInput = document.getElementById('system-input');
+let userButton = document.getElementById('user-button');
+let assistantButton = document.getElementById('assistant-button');
+let messageContainer = document.getElementById('message-container');
 let bodyElement = document.getElementsByTagName('body')[0];
 let title = document.querySelector("h1");
-
+let messages = [(systemInput.placeholder !== '') ? systemInput.placeholder : systemInput.value];
 let paneIsOpen = false; 
 
-// Load settings from local storage at start
 window.onload = function() {
     let storedKey = localStorage.getItem("openAIKey");
     if(storedKey) { 
@@ -59,7 +61,7 @@ overlay.addEventListener("click", function () {
     }, 500);
     button.classList.remove("active");
     paneIsOpen = false;
-    // When pane closes, save the input field value and dark mode state to local storage
+
     localStorage.setItem("openAIKey", inputField.value);
     localStorage.setItem("darkMode", darkModeToggle.checked);
 });
@@ -73,12 +75,138 @@ slider.addEventListener("click", function() {
         systemInput.classList.add("dark-mode");
         title.classList.add("dark-mode");
         button.classList.add("dark-mode");
+
+        const textAreasDark = document.querySelectorAll('.new-input');
+        textAreasDark.forEach(textArea => {
+            textArea.classList.add('dark-mode');
+        });
+
         localStorage.setItem("darkMode", 'true');
     } else {
         bodyElement.classList.remove("dark-mode");
         title.classList.remove("dark-mode");
         button.classList.remove("dark-mode");
         systemInput.classList.add("dark-mode");
+
+        const textAreasLight = document.querySelectorAll('.new-input');
+        textAreasLight.forEach(textArea => {
+            textArea.classList.remove('dark-mode');
+        });
+
         localStorage.setItem("darkMode", 'false');
     }
+});
+
+function autoExpand(element) {
+    // Only adjust the height if there's an overflow
+    if (element.scrollHeight > element.clientHeight) {
+        element.style.height = element.scrollHeight + 'px';
+    }
+}
+function createNewTextArea(role, defaultText = '') {
+    let textarea = document.createElement('textarea');
+    let textareaWrapper = document.createElement('div'); 
+    let removeButton = document.createElement('button');
+
+    textarea.dataset.role = role;
+    textareaWrapper.classList.add('textarea-wrapper', 'fadeIn');
+    textarea.classList = 'new-input';
+    textarea.placeholder = role;
+    textarea.value = defaultText; // Set default text
+    removeButton.classList.add('remove-textarea'); 
+    removeButton.innerText = '—'; 
+    removeButton.title = "Remove this box"; 
+    
+    textareaWrapper.appendChild(textarea);
+    textareaWrapper.appendChild(removeButton);
+    messageContainer.appendChild(textareaWrapper);
+
+    let oldText = defaultText;  // this will hold the previous input value
+
+    messages.push(oldText);
+    autoExpand(textarea);
+
+    textarea.addEventListener('input', () => {
+        let newText = textarea.value;
+        const index = messages.indexOf(oldText);
+        if (index !== -1) {
+            messages[index] = newText;
+        }
+        oldText = newText;
+        textareaWrapper.style.height = `${textarea.scrollHeight + 40}px`;
+        autoExpand(textarea);
+        console.log(messages);
+    });
+
+    textareaWrapper.style.height = `${textarea.scrollHeight + 40}px`;
+
+    removeButton.addEventListener('click', function () {
+        textareaWrapper.style.animation = "fadeOut 0.6s ease-in forwards";
+        setTimeout(function(){
+            messageContainer.removeChild(textareaWrapper);
+        }, 600);
+
+        const index = messages.indexOf(oldText);
+        if (index !== -1) {
+            messages.splice(index, 1);
+        }
+
+        console.log(messages);
+    });
+}
+
+userButton.addEventListener("click", function(event) {
+    createNewTextArea('User');
+});
+
+assistantButton.addEventListener("click", function(event) {
+    createNewTextArea('Assistant');
+});
+
+systemInput.addEventListener('input', function () {
+    messages[0] = systemInput.value;
+});
+
+document.getElementById('send-button').addEventListener('click', function() {
+    // map the messages array to the needed format
+    const formattedMessages = messages.map((message, index) => {
+        let role;
+        if (index === 0) {
+            role = 'system';
+        } else {
+            const textarea = document.querySelectorAll('.new-input')[index - 1];
+            role = textarea.dataset.role.toLowerCase();
+        }
+        return {
+            role,
+            content: message
+        };
+    });
+
+    // prepare the body of the request
+    const requestBody = {
+        model: 'gpt-4',
+        messages: formattedMessages
+    };
+
+    // use Fetch API to send a request
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${inputField.value}` // get the API Key from the settings
+        },
+        body: JSON.stringify(requestBody),
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data['choices'] && data['choices'][0] && data['choices'][0]['message']) {
+                createNewTextArea('Assistant', data['choices'][0]['message']['content']);
+            } else {
+                console.error('API response does not contain the assistant’s reply.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 });
