7bcb0ae6d2878036ae840fe0038bd2827d2ed4de
uink45 <79078981+uink45@users.noreply.github.com>
2023-10-06 16:24:01 +1000

    Add animations.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 let button = document.querySelector(".settings-icon");
+let settingsPane = document.getElementById('settings-pane');
+let overlay = document.querySelector('#overlay');
+let paneIsOpen = false;
 
 button.addEventListener("click", function(event) {
-    event.stopPropagation(); // Prevent this click from triggering the document's click event
-    button.classList.add("active");
+    event.stopPropagation();
+    if(!paneIsOpen) { 
+        settingsPane.style.display = 'block';
+        overlay.style.display = 'block';
+        button.classList.add("active");
+        // Remove 'exit' class to reverse the animation
+        settingsPane.classList.remove("exit");
+        overlay.classList.remove("exit");
+        paneIsOpen = true; 
+    } else {
+        // Assign 'exit' class to start the animation
+        settingsPane.classList.add("exit");
+        overlay.classList.add("exit");
+        // Delay hiding the elements to let animation finish
+        setTimeout(function(){
+            settingsPane.style.display = 'none';
+            overlay.style.display = 'none';
+        }, 500); // same duration as your CSS animation
+        button.classList.remove("active");
+        paneIsOpen = false;
+    }
 });
 
-// Click anywhere to remove the active class on the button
-document.addEventListener("click", function() {
+// Click anywhere to close the settings pane
+overlay.addEventListener("click", function () {
+    settingsPane.classList.add("exit");
+    overlay.classList.add("exit");
+    setTimeout(function(){
+        settingsPane.style.display = 'none';
+        overlay.style.display = 'none';
+    }, 500);
     button.classList.remove("active");
+    paneIsOpen = false; 
 });
