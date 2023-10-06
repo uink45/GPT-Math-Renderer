52aebb8556efd6feac0462c9a5148ffb3a09f8f3
uink45 <79078981+uink45@users.noreply.github.com>
2023-10-06 15:33:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let button = document.querySelector(".settings-icon");
 
-button.addEventListener("click", function() {
-    button.classList.toggle("active");
+button.addEventListener("click", function(event) {
+    event.stopPropagation(); // Prevent this click from triggering the document's click event
+    button.classList.add("active");
+});
+
+// Click anywhere to remove the active class on the button
+document.addEventListener("click", function() {
+    button.classList.remove("active");
 });
